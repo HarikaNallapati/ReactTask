d463f6ea40d11dd6b7e9824bd15217a41d655535
HarikaNallapati <150787211+HarikaNallapati@users.noreply.github.com>
2024-07-30 11:35:20 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,23 @@
+
+import './App.css';
+import "pepsico-ds/css";
+import {Button, Header} from "pepsico-ds";
+import {DatePickerInput} from "pepsico-ds";
+import HeaderSignIn from './components/HeaderSignIn';
+import BasicForm from './components/BasicForm';
+import {Icon} from "pepsico-ds";
+
+function App() {
+  return (
+    <div className= 'container'>
+      <HeaderSignIn/>
+    <div className='PepForm'>
+    <BasicForm/>
+
+    </div>
+    </div>
+    
+  );
+}
+
+export default App;
